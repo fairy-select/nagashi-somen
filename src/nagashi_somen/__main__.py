d3055642e6613e1826880afd4996fedd20c9a2af
Noritaka IZUMI <noritaka.izumi@gmail.com>
2025-11-01 11:55:37 +0900

    create new (for cli)

---
+++ src/nagashi_somen/__main__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025-present Noritaka IZUMI <noritaka.izumi@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+import sys
+
+if __name__ == "__main__":
+    from nagashi_somen.cli import nagashi_somen
+
+    sys.exit(nagashi_somen())
